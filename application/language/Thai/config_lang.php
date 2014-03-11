224cd27f7f305a1d0b4d17bdda06e139b6bf6f3e
Padungsak <suntonpon@hotmail.com>
2014-03-11 10:53:12 +0700

    First commit
    Project first draph

---
+++ application/language/Thai/config_lang.php
@@ -0,0 +1,35 @@
+<?php
+$lang['config_info']='องค์ประกอบร้านค้า';
+$lang['config_company']='ชื่อร้านค้า';
+$lang['config_address']='ที่อยู่';
+$lang['config_phone']='เบอร์โทรศัพท์';
+$lang['config_website']='เว็บไซต์';
+$lang['config_fax']='แฟ็ก';
+$lang['config_default_tax_rate']='อัตตราภาษีเริ่มต้น %';
+$lang['config_default_tax_rate_1']='อัตตราภาษี 1';
+$lang['config_default_tax_rate_2']='อัตตราภาษี 2';
+$lang['config_company_required']='ชื่อร้านค้าต้องกรอก';
+$lang['config_address_required']='ที่อยู่ต้องกรอก';
+$lang['config_phone_required']='เบอร์โทรต้องกรอก';
+$lang['config_default_tax_rate_required']='อัตตราภาษีเริ่มต้นต้องกรอก';
+$lang['config_default_tax_rate_number']='อัตตราภาษีเริ่มต้นต้องเป็นตัวเลข';
+$lang['config_company_website_url']='เว็บไซต์ร้านค้าไม่ถูกต้อง';
+$lang['config_saved_successfully']='องค์ประกอบร้านค้าบันทึกเรียบร้อย';
+$lang['config_saved_unsuccessfully']='องค์ประกอบร้านค้าบันทึกล้มเหลว';
+$lang['config_return_policy_required']='กฎการคืนของต้องกรอก';
+$lang['config_print_after_sale']='ปริ๊นบิลหลังการขาย';
+$lang['config_language'] = 'ภาษา';
+$lang['config_timezone'] = 'Timezone';
+$lang['config_currency_symbol'] = 'ลัญลักษณ์ค่าเงิน';
+$lang['config_currency_side'] = 'Right side'; //GARRISON ADDED 4/20/2013
+$lang['config_custom1'] = 'Custom Field 1'; //GARRISON ADDED 4/21/2013
+$lang['config_custom2'] = 'Custom Field 2'; //GARRISON ADDED 4/21/2013
+$lang['config_custom3'] = 'Custom Field 3'; //GARRISON ADDED 4/21/2013
+$lang['config_custom4'] = 'Custom Field 4'; //GARRISON ADDED 4/21/2013
+$lang['config_custom5'] = 'Custom Field 5'; //GARRISON ADDED 4/21/2013
+$lang['config_custom6'] = 'Custom Field 6'; //GARRISON ADDED 4/21/2013
+$lang['config_custom7'] = 'Custom Field 7'; //GARRISON ADDED 4/21/2013
+$lang['config_custom8'] = 'Custom Field 8'; //GARRISON ADDED 4/21/2013
+$lang['config_custom9'] = 'Custom Field 9'; //GARRISON ADDED 4/21/2013
+$lang['config_custom10'] = 'Custom Field 10'; //GARRISON ADDED 4/21/2013
+?>
